4532c18d9434a31ce3c249a3a7f3b74328ab2ba9
Angsar Aidarbek <aidarbek.ansar@mail.ru>
2024-03-19 09:56:20 +0500

    print statements added

--- main.py
+++ main.py
@@ -17,31 +17,55 @@ exec(open(fullPath + "/9_NMSLIB/main.py").read())
 
 naming = ['glove-25', 'glove-50', 'glove-100', 'glove-200', 'nytimes-256', 'sift-128', 'lastfm-64']
 problematic = ['deepImage-96', 'gist-960']
-methods = ['annoy', 'sclearn_ballTree', 'sclearn_kdTree', 'mrpt', 'hnswlib', 'faiss_hnsw', 'faiss_ivf', 'pynndescent', 'scipy', 'nmslib']
+methods = ['annoy', 'sclearn_ballTree', 'sclearn_kdTree', 'mrpt', 'hnswlib', 'faiss_hnsw', 'faiss_ivf', 'scipy', 'nmslib'] #'pynndescent'
 datasets_list = ['deepImage-96', 'fashionMnist-784', 'gist-960', 'glove-25', 'glove-50', 'glove-100', 'glove-200', 'mnist-784', 'nytimes-256', 'sift-128', 'lastfm-64']
 results = []
 metric = 'euclidean'
 example = ['mnist-784', 'fashionMnist-784']
-runs = 10
+runs = 1
 queries = 1000
-for name in naming:
+for name in problematic:
     result = []
-    result.append(annoy_run(name, metric, runs, queries))
-    result.append(sclearn_run(name, metric, runs, queries, 'ball_tree'))
-    result.append(sclearn_run(name, metric, runs, queries, 'kd_tree'))
-    result.append(mrpt_run(name, metric, runs, queries))
-    result.append(hnsw_run(name, metric, runs, queries))
-    result.append(faiss_run(name, metric, runs, queries, 'hnsw'))
-    result.append(faiss_run(name, metric, runs, queries, 'ivf'))
-    result.append(pynndescent_run(name, metric, runs, queries))
-    result.append(scipy_run(name, metric, runs, queries))
-    result.append(nmslib_run(name, metric, runs, queries))
+    annoy = annoy_run(name, metric, runs, queries)
+    print(annoy)
+    result.append(annoy)
+    sclearnBallTree = sclearn_run(name, metric, runs, queries, 'ball_tree')
+    print(sclearnBallTree)
+    result.append(sclearnBallTree)
+    sclearnKdTree = sclearn_run(name, metric, runs, queries, 'kd_tree')
+    print(sclearnKdTree)
+    result.append(sclearnKdTree)
+    mrpt = mrpt_run(name, metric, runs, queries)
+    print(mrpt)
+    result.append(mrpt)
+    hnswLib = hnsw_run(name, metric, runs, queries)
+    print(hnswLib)
+    result.append(hnswLib)
+    faissHnsw = faiss_run(name, metric, runs, queries, 'hnsw')
+    print(faissHnsw)
+    result.append(faissHnsw)
+    faissIvf = faiss_run(name, metric, runs, queries, 'ivf')
+    print(faissIvf)
+    result.append(faissIvf)
+    # pyNNDescent = pynndescent_run(name, metric, runs, queries)
+    # print(pyNNDescent)
+    # result.append(pyNNDescent)
+    scippy = scipy_run(name, metric, runs, queries)
+    print(scippy)
+    result.append(scippy)
+    nmslibNhsw = nmslib_run(name, metric, runs, queries)
+    print(nmslibNhsw)
+    result.append(nmslibNhsw)
     results.append(result)
+    for j in range(len(result)):
+        if j < len(methods) :
+            print(methods[j], end=', ')
+        print(result[j])
 
-for i in range(len(naming)):
-    print(naming[i] + "start ---------------------------------------")
+for i in range(len(problematic)):
+    print(problematic[i] + "start ---------------------------------------")
     for j in range(len(results[i])):
-        if i < len(methods) :
-            print(methods[i], end=', ')
+        if j < len(methods) :
+            print(methods[j], end=', ')
         print(results[i][j])
-    print(naming[i] + "end ---------------------------------------")
+    print(problematic[i] + "end ---------------------------------------")
